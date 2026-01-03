8c2662a7fa818fc1daf4924f58394e699faa70b6
stbsx <iliast3000@gmail.com>
2026-01-03 20:38:39 +0500

    as
    asd

--- Assets/_Scenes/IlyaStepanovOlegovich/Player/CameraFollow.cs
+++ Assets/_Scenes/IlyaStepanovOlegovich/Player/CameraFollow.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public Vector3 offset = new Vector3(0, 5, -10);
+    public Vector3 offset = new Vector3(0, 4, 1);
     public float smoothTime = 0.2f; // чем меньше, тем резче, чем больше, тем плавнее
 
     private Vector3 velocity = Vector3.zero;
